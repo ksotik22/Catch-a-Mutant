499926b0e6cf8f2cec8cf9a725dd36ca7b2d0150
ksotik22 <nikf4610.can@gmail.com>
2026-09-05 18:33:05 +0300

    Spawn new mutant species

--- scripts/player.gd
+++ scripts/player.gd
@@ -22,6 +22,8 @@ func _install_visual_upgrade() -> void:
     get_tree().current_scene.add_child(concept_upgrade)
     var mechanics = preload("res://scripts/world_mechanics.gd").new()
     get_tree().current_scene.add_child(mechanics)
+    var mutant_spawner = preload("res://scripts/mutant_spawner.gd").new()
+    get_tree().current_scene.add_child(mutant_spawner)
     await get_tree().process_frame
     var animation_controller = preload("res://scripts/player_animation.gd").new()
     animation_controller.name = "PlayerAnimation"
