287bb6969038bd82675a3d3931eec4f677326775
ksotik22 <nikf4610.can@gmail.com>
2026-09-05 18:49:25 +0300

    Add rarity-based mutant sell value

--- scripts/game_state.gd
+++ scripts/game_state.gd
@@ -5,18 +5,20 @@ signal coins_changed(total: int)
 
 var catches: int = 0
 var coins: int = 0
-var mutant_sell_price: int = 10
+var stored_value: int = 0
 
-func add_catch() -> void:
+func add_catch(value: int = 10) -> void:
     catches += 1
+    stored_value += value
     catches_changed.emit(catches)
 
 func sell_all() -> int:
     var sold := catches
     if sold <= 0:
         return 0
-    coins += sold * mutant_sell_price
+    coins += stored_value
     catches = 0
+    stored_value = 0
     catches_changed.emit(catches)
     coins_changed.emit(coins)
     return sold
