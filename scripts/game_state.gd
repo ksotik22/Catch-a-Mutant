726ef70610945ae203ccb9dc9ad62c4912b39554
ksotik22 <nikf4610.can@gmail.com>
2026-09-05 18:30:43 +0300

    Add coins and selling caught mutants

--- scripts/game_state.gd
+++ scripts/game_state.gd
@@ -1,8 +1,22 @@
 extends Node
 
 signal catches_changed(total: int)
-var catches := 0
+signal coins_changed(total: int)
+
+var catches: int = 0
+var coins: int = 0
+var mutant_sell_price: int = 10
 
 func add_catch() -> void:
     catches += 1
     catches_changed.emit(catches)
+
+func sell_all() -> int:
+    var sold := catches
+    if sold <= 0:
+        return 0
+    coins += sold * mutant_sell_price
+    catches = 0
+    catches_changed.emit(catches)
+    coins_changed.emit(coins)
+    return sold
